193cb6819c98629adfa45eb6b91ee7ff7eef18e5
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2021-04-19 22:48:17 +0200

    plus

---
+++ Sources/5. Numbers.swift
@@ -0,0 +1,17 @@
+indirect enum Num { 
+   case zero
+   case onemorethan(Num)
+}
+
+let a: Num = Num.zero
+let b: Num = .onemorethan(.zero)
+let c: Num = .onemorethan(.onemorethan(.zero))
+
+func plus(_ a: Num, _ b: Num) -> Num {
+   switch a {
+      case .zero :
+         return b 
+      case .onemorethan(let n):
+         return Num.onemorethan(plus(n,b))
+   }
+}
