d9d5aa34138f138cb4dcb90d479dc9e76953efd7
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2021-04-19 21:40:14 +0200

    ok

--- Sources/2.Types with two things.swift
+++ Sources/2.Types with two things.swift
@@ -9,3 +9,16 @@ let salt = Seasoning.salt
 let seasonings = [pepper, salt, .pepper, .salt]
 let moarseasonings: [Seasoning] = [.salt, .pepper, .pepper, .salt]
 seasonings
+
+
+func salty(_ s: Seasoning)  -> Bool { 
+  switch s {
+     case .salt:
+        return true
+     case .pepper:
+        return false 
+  } 
+} 
+
+salty(salt)
+salty(pepper)
